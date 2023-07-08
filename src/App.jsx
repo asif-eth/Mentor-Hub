7562052bac90761e7e642ba320f5d8781b9baeab
asifkhan <khanasifxyz@gmail.com>
2023-07-08 06:27:53 +0530

    Footer Completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Body from './components/Body/Body'
 import Community from './components/Community/Community'
 import Explore from './components/Explore/Explore'
 import Feedback from './components/Feedback/Feedback'
+import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
     <Explore />
     <Community />
     <Feedback />
+    <Footer />
     </>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <>
+    <div className="footerBody">
+        <div className="footerArea">
+
+            <div className="footer__logoSection">
+                <img src="/Rectangle 3.jpg" alt="" />
+                <div className="footer__socialsLogo">
+                    <img src="/faceBookLogo.svg" alt="FaceBook" />
+                    <img src="/twitterLogo.svg" alt="Twitter" />
+                    <img src="/linkedinLogo.svg" alt="LinkedIn" />
+                    <img src="/instagramLogo.svg" alt="Instagram" />
+                </div>
+            </div>
+
+            <div className="footer__pagesSection">
+                <h4>Pages</h4>
+                <div className="footer__pages">
+                    <ul>
+                        <li>About Us</li>
+                        <li>Mentors</li>
+                        <li>Sessions</li>
+                        <li>Sign up</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="footer__contactSection">
+                <h4>Contact</h4>
+                <div className="footer__contacts-Address">
+                    <p>123 Main Street</p>
+                    <p>City, State 12345</p> 
+                </div>
+                <div className="footer__contact-Mail">
+                    <a href="mailto:contact@mentorhub.com">contact@mentorhub.com</a>
+                </div>
+                <div className="footer__contact-Number">(123) 456 - 7890</div>
+            </div>
+
+        </div>
+        <div className="footer__rightsSection">
+                <p>© 2023 Mentor Hub. All rights reserved.</p>
+            </div>
+    </div>
+    </>
+  )
+}
+
+export default Footer
