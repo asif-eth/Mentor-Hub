3280c772c62f612fe6f0b8afbd4145b5a3e0ac7f
asifkhan <khanasifxyz@gmail.com>
2023-07-10 22:56:25 +0530

    Implemented Hashrouter To Tackle Error In Deployment

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,19 @@ import Feedback from './components/Feedback/Feedback'
 import Footer from './components/Footer/Footer'
 import Home from './pages/Home/Home'
 import Mentor from './pages/Mentor/Mentor'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
 import Form from './components/Form/Form'
 
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route path='/' element={[<Home />, <Body /> , <Explore />, <Community />, <Feedback />, <Footer />]} />
       <Route path='/mentor' element={<Mentor />} />
       <Route path='/form' element={<Form />} />
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
