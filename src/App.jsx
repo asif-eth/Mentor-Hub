04f8ff95e3b486c99781e6a503685b873b801c12
asifkhan <khanasifxyz@gmail.com>
2023-07-10 23:04:15 +0530

    Implemented HashRouter And Tackled The Deployment Issue

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,18 @@ import Feedback from './components/Feedback/Feedback'
 import Footer from './components/Footer/Footer'
 import Home from './pages/Home/Home'
 import Mentor from './pages/Mentor/Mentor'
-import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Form from './components/Form/Form'
 
 function App() {
 
   return (
     <HashRouter>
-    <Routes>
-      <Route path='/' element={[<Home />, <Body /> , <Explore />, <Community />, <Feedback />, <Footer />]} />
-      <Route path='/mentor' element={<Mentor />} />
-      <Route path='/form' element={<Form />} />
-    </Routes>
+      <Routes>
+        <Route path='/' element={[<Home />, <Body /> , <Explore />, <Community />, <Feedback />, <Footer />]} />
+        <Route path='/mentor' element={<Mentor />} />
+        <Route path='/form' element={<Form />} />
+      </Routes>
     </HashRouter>
   )
 }
